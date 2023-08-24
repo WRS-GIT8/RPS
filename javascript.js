a998994fba4b0b1d6a7e89e2acc1e6215cdd329a
William Stansbury <136635602+WRS-GIT8@users.noreply.github.com>
2023-08-24 09:38:00 -0400

    Add main game logic for detirmining the winner with the play function.

--- javascript.js
+++ javascript.js
@@ -2,14 +2,38 @@ function getComputerChoice(){
     let gameChoice
     let compChoice = Math.floor(Math.random() * 3)
     if (compChoice == 0) {
-        gameChoice = "Rock"
+        gameChoice = "rock"
     } else if (compChoice == 1) {
-        gameChoice = "Paper"
+        gameChoice = "paper"
     } else {
-        gameChoice = "Scissor"
+        gameChoice = "scissors"
     }
-    console.log(gameChoice)
+    return gameChoice
 }
 
-getComputerChoice()
+function getPlayerChoice(){
+    let pChoice = prompt("Please choose rock, paper, or scissors")
+    pChoice = pChoice.toLowerCase()
+    return pChoice
+}
 
+function play(playerSelection, computerSelection){
+    if (playerSelection === computerSelection) {
+        console.log("It is a tie!")
+        return "It is a tie!"
+    }
+    else if (
+        (playerSelection === "rock" && computerSelection === "scissors") ||
+        (playerSelection === "paper" && computerSelection === "rock") ||
+        (playerSelection === "scissors" && computerSelection === "paper")
+        ) {
+        console.log("You win!")
+        return "You win!"
+        }
+    else {
+        console.log("You lose...")
+        return "The computer wins! ${computerChoice} beats ${playerChoice}"
+    }
+}
+
+play(getPlayerChoice(), getComputerChoice())
